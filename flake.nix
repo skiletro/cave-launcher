7f63eae5a4b1b3035acb58e2bcaf8af15911d9c1
jamie <git@skilet.ro>
2025-10-28 14:30:19 +0000

    update flake

--- flake.nix
+++ flake.nix
@@ -5,15 +5,15 @@
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    inputs@{ flake-parts, self, ... }:
+    flake-parts.lib.mkFlake { inherit inputs self; } {
       systems = [
         "aarch64-darwin"
         "x86_64-linux"
       ];
 
       perSystem =
-        { pkgs, ... }:
+        { pkgs, lib, ... }:
         {
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
@@ -26,16 +26,26 @@
 
           packages =
             let
-              attrs = flags: {
+              ss = lib.substring;
+              lmd = self.lastModifiedDate;
+
+              attrs = rec {
                 pname = "cave-assistant";
                 version = "0.1.0";
                 src = ./.;
                 vendorHash = "sha256-7OhM7t0BRnmpEtvuEt/AuxklB20NT+ivvrTYe5KibEs=";
+                ldflags = [
+                  "-X 'main.VERSION=v${version}'" # for ver number in titlebar
+                  "-X 'main.LAST_MODIFIED=Built ${ss 6 2 lmd}/${ss 4 2 lmd}/${ss 0 4 lmd}'"
+                  "-s" # omits symbol table
+                  "-w" # omits DWARF debug info
+                  "-H=windowsgui"
+                ];
               };
             in
             {
               default = pkgs.buildGoModule attrs;
-              windows = pkgs.pkgsCross.mingwW64.buildGoModule attrs // { ldflags = "-H=windowsgui";};
+              windows = pkgs.pkgsCross.mingwW64.buildGoModule attrs;
             };
         };
     };
